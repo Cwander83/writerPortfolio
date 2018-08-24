a4c8f2fe26e5e1d40761829269d223458170a30a
Cwander83 <chriswandermail@gmail.com>
2018-08-24 15:40:42 -0400

    building story modals

--- js/app.js
+++ js/app.js
@@ -1,41 +1,90 @@
-$(function () {
-
-
+$(function() {
 	modalTitleCreate();
+
+	$('[data-toggle="popover"]').popover();
 });
 
 // CREATING THE CLICKABLE LINK TO OPEN THE MODAL FOR EACH STORY
 function modalTitleCreate() {
-
 	const $titlesArray = [];
 
 	for (let i = 0; i < stories.length; i++) {
-
-		$titlesArray.push($("<h1>", {
-			text: stories[i].title
-		}));
+		$titlesArray.push(
+			$("<h1>", {
+				text: stories[i].title,
+				"data-toggle": "popover",
+				"data-trigger": "hover",
+				"data-container": "body",
+				"data-placement": "top",
+				"data-content": "Preview: " + stories[i].firstSentence
+			})
+		);
 	}
 
-
 	$("#storyHeadings").append($titlesArray);
-	$("#storyHeadings h1").wrap(
-		"<a data-toggle='modal'></a>"
-	);
-	$("#storyHeadings a").each(function (j) {
-		$(this).attr('data-target', '#storyModal' + j);
-	})
 
-	storyHeaderPopover();
+	$("#storyHeadings h1").wrap("<a data-toggle='modal'></a>");
+
+	$("#storyHeadings a").each(function(j) {
+		$(this).attr("data-target", "#storyModal" + j);
+	});
+	console.log("titlecreate");
+	modalBodyCreate();
 }
 
-function storyHeaderPopover() {
-	$.each(stories, function () {
-		// THIS IS WHERE TO START ADDING THE POPOVER TO THE H1 TAG FROM PREVIOUS FUNCTION ============================================================
-	})
-
-}
-
-
-// TAKES THE STORIES JSON AND BUILDS THE MODAL BODY THE 
+// TAKES THE STORIES JSON AND BUILDS THE MODAL BODY THE
 // ASYNC FUNCTION ABOVE MODALTITLECREATE() BUILDS
-function modalBodyCreate() {}
+function modalBodyCreate() {
+	const $modal = [];
+	const $storyIndex = [];
+	const $storyTitle = [];
+	const $stories = [];
+	console.log($storyTitle);
+
+	for (let i = 0; i < stories.length; i++) {
+		$modal.push(
+			$("<div>", {
+				id: "storyModal" + i,
+				class: "modal fade",
+				role: "dialog",
+				"aria-hidden": true
+			})
+		);
+		$storyIndex.push(i);
+		$storyTitle.push(stories[i].title);
+		$stories.push(stories[i].story);
+	}
+
+	$("#storyModals").append($modal);
+
+	$(".modal").each(function() {
+		$(this)
+			.append(
+				$("<div>", {
+					class: "modal-dialog modal-lg",
+					role: "document"
+				})
+			)
+			.append(
+				$("<div>", {
+					class: "modal-content"
+				})
+			);
+	});
+
+	$(".modal-content").each(function(i) {
+		$(this)
+			.append(
+				$("<div>", {
+					class: "modal-header",
+					text: $storyTitle[i]
+				})
+			)
+			.append(
+				$("<div>", {
+					class: "modal-body",
+					text: $stories[i]
+				})
+			);
+	});
+}
