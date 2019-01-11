d92c2e83dc03ed866acbe35fecddcfc07352c3b0
Chris Wander <31929558+Cwander83@users.noreply.github.com>
2019-01-11 11:19:50 -0500

    fixed aobut image css in landscape mobile

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(function() {
 	$(window).scroll(function() {
+
 		/* affix after scrolling 100px */
 		if ($(document).scrollTop() > 100) {
 			$("nav").addClass("shrink");
@@ -7,15 +8,20 @@ $(function() {
 			$("nav").removeClass("shrink");
 		}
 	});
+
 	detectmob();
+	
 	// HIDING THE POPOVER IN A MOBILE VIEWPORT
 	function detectmob() {
+
 		if (window.innerWidth <= 800) {
 			$('[data-toggle="popover"]').popover("hide");
+
 			console.log("if popover");
 		} else {
 			return $('[data-toggle="popover"]').popover();
 		}
+
 		console.log("else popover");
 	}
 });
